9300755206f7aae6845f23bf169996d82676b97a
unknown <natalia.duran@labinfo.is.escuelaing.edu.co>
2020-02-27 08:56:09 -0500

    Paginas agregadas

--- src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
@@ -6,15 +6,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest()
+//@RunWith(SpringRunner.class)
+//@SpringBootTest()
 public class ApplicationServicesTests {
     
-    @Test
+    /*@Test
     public void contextLoads() { 
-    }
-
+    }*/
 }
